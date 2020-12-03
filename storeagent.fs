56eaa350a034848c49be900abb3717b939e6c1d2
Mastracchio, Ugo <VHRD47@zebra.com>
2020-12-03 15:12:25 +0100

    pointing now to the correct json files

--- storeagent.fs
+++ storeagent.fs
@@ -1,4 +1,4 @@
-﻿module StoreAgent
+module StoreAgent
 
 
 open System
@@ -89,7 +89,7 @@ type StoreAgent() =
                       }
             // http://fsharp.github.io/FSharp.Data/library/Http.html
             // let defaultjson = Http.RequestString("http://weblinkendpoint.mastracu.it/defaultinventory.json")
-            let defaultjson =File.ReadAllText("defaultinventory.json")
+            let defaultjson =File.ReadAllText("./json/defaultinventory.json")
             let newStore = { ProductList = Array.toList (unjson<Product array> defaultjson) } 
             storeAgentLoop newStore
 
