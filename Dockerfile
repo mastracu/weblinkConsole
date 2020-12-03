948c50ffe37965c1c4fc8fb3f9e8ab54b1559971
Mastracchio, Ugo <VHRD47@zebra.com>
2020-12-03 18:10:16 +0100

    now with docker-buildandrun

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mcr.microsoft.com/dotnet/aspnet:5.0
 COPY bin/Release/net5.0/publish/ App/
 WORKDIR /App
-EXPOSE 8080
+EXPOSE 8083
 ENTRYPOINT ["dotnet", "suaveNetCore5.dll"]
 
---
+++ docker-buildandrun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1 = weblinkendpoint
+echo "building container: docker build -t $1 ."
+docker build -t $1 .
+echo "stopping all containers"
+docker stop $(docker ps -a -q)
+echo "removing all untagged images"
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }'  | xargs -r docker rmi
+echo "starting container:"
+docker run -d \
+  -it \
+  -p8083:8083 \
+  --mount type=bind,source="$(pwd)"/../zebrafirmware,target=/src/firmware \
+  $1
