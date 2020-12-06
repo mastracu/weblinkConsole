f900e1f8756012dc10a5fb02ad30c5db1a1facbc
Mastracchio, Ugo <VHRD47@zebra.com>
2020-12-06 16:50:59 +0100

    force option in container prune

--- docker-buildandrun.sh
+++ docker-buildandrun.sh
@@ -5,7 +5,7 @@ docker build -t $1 .
 echo "stopping all containers"
 docker stop $(docker ps -a -q)
 echo "removing stopped containers"
-docker container prune
+docker container prune -f
 echo "removing all untagged images"
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }'  | xargs -r docker rmi
 echo "starting container:"
