c091f4f4a5bda1dda8187fa67f72fec752761abf
Mastracchio, Ugo <VHRD47@zebra.com>
2022-10-07 11:56:49 +0200

    DHL label application published

--- identityStore.fs
+++ identityStore.fs
@@ -12,18 +12,18 @@ type IdentityRecord = {
     Company: string
 }
 
-let private identityStorage = new Dictionary<string, IdentityRecord>()
+//let private identityStorage = new Dictionary<string, IdentityRecord>()
 
-let jsonStr =File.ReadAllText(Path.GetFullPath "./json/identity.json")
-Array.toList (unjson<PrinterApp array> jsonStr)
+//let jsonStr =File.ReadAllText(Path.GetFullPath "./json/identity.json")
+//Array.toList (unjson<PrinterApp array> jsonStr)
 
-let getClaims userName =
-    seq {
-        yield (ClaimTypes.Name, userName)
-        yield (ClaimTypes.Role, identityStorage.[userName].Company)
-    } |> Seq.map (fun x -> new Claim(fst x, snd x)) |> async.Return
+//let getClaims userName =
+//    seq {
+//        yield (ClaimTypes.Name, userName)
+//        yield (ClaimTypes.Role, identityStorage.[userName].Company)
+//    } |> Seq.map (fun x -> new Claim(fst x, snd x)) |> async.Return
 
-let isValidCredentials username password =
-    match identityStorage.ContainsKey(username) with 
-    | false -> false |> async.Return
-    | true ->  (identityStorage.[username]).Password = password |> async.Return
+//let isValidCredentials username password =
+//    match identityStorage.ContainsKey(username) with 
+//    | false -> false |> async.Return
+//    | true ->  (identityStorage.[username]).Password = password |> async.Return
