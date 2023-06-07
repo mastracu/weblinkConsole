3e4d9626282532e09987178dc8620343e10c5459
Mastracchio, Ugo <VHRD47@zebra.com>
2023-06-07 21:17:22 +0200

    no stop and rm docker procedure until it's fixed

--- docker-buildandrun.sh
+++ docker-buildandrun.sh
@@ -2,14 +2,14 @@
 # $1 = weblinkendpoint
 echo "building container: docker build -t $1 ."
 docker build -t $1 .
-echo "stopping all containers"
-docker stop $(docker ps -f ancestor=$1 -a -q)
-echo "removing stopped containers"
-docker container prune -f
-echo "removing all untagged images"
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }'  | xargs -r docker rmi
-echo "starting container:"
-docker run -d \
+# echo "stopping all containers"
+# docker stop $(docker ps -f ancestor=$1 -a -q)
+# echo "removing st opped containers"
+# docker container#  prune -f
+# echo "removing a# ll untagged images"
+# docker images --# no-trunc | grep '<none>' | awk '{ print $3 }'  | xargs -r docker rmi
+echo "starting c# ontainer:"
+docker run -d#  \
   -it \
   -p8083:8083 \
   --mount type=bind,source="$(pwd)"/../zebrafirmware,target=/App/firmware \
